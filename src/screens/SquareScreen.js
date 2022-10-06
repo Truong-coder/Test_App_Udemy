9222a9e13fdff087c2912677fcd8be6f18fa95b1
Truong-coder <dinhtruong2k2k@gmail.com>
2022-10-07 02:36:17 +0700

    first commit

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import { View, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
@@ -11,52 +11,36 @@ of syntax as a sign to others that this is some very special variable
 const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
+    const reducer = (state, action) => {
+    // state === {red: number, green: number, blue: number };
+    // action === {colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15 }
+   
+    switch (action.colorToChange) {
+        // never modify a state dỉrectly
+        case 'red':
+    /**
+     * Make a new object
+     * Take all the existing props out of our state object and
+     * we're going to essentially copy paste them into new one
+     * Then overwriting the existing red property and the new value
+     * for red will be state.red + action.amount
+     */
+            return { ...state, red: state.red + action.amount};
+        case 'green':
+            return { ...state, green: state.green + action.amount};
+        case 'blue':
+            return { ...state, blue: state.blue + action.amount};
+    /**
+     * The dafault case is going to handle the case in which we accidentally throw an action
+     With a invalid color to change or a undefined color to change
+     *  */ 
+        default: 
+    }
+};
     // create state values color from 0 - 255
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
-
-    
-    const setColor = (color, change) => {
-        // color === 'red', 'green', 'blue'
-        // change === +15, -15
-        
-        switch (color) {
-            case 'red':
-        /* TERNARY EXPRESSION
-        - A ternary expression is going to essentially check on statement
-        - If that statement evaluates to a truth value, it will run some immediate statement 
-        in this case is 'return' otherwise it runs statement 'setRed(red + change)'
-        - The '?' means that this is going to be the expression that we want to run if all this evaluates to a truth value
-        - The ':' means do this if the previous statement false    
-
-        */
-                red + change > 255 || red + change < 0 ? null : setRed(red+change) 
-                // if(color === 'red'){
-                //     if(red + change > 255 || red + change < 0){
-                //         return;
-                //     }
-                //     else{
-                //         setRed(red + change);
-                //     }
-                // } 
-                return; 
-            case 'green':
-                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-                return;
-            case 'blue':
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-                return;
-                    
-              
-        }
-        
-    };
-
-    console.log(red);
-    console.log(green);
-    console.log(blue);
-
+    // pass in reducer function
+    const [state, dispatch] = useReducer(reducer, {red:0, green: 0, blue: 0});
+    //define reducer function
     return(
         <View>
             {/* -JSX 
@@ -69,18 +53,18 @@ const SquareScreen = () => {
                 // onIncrease = {()=> {setRed(red+COLOR_INCREMENT)}} 
                 // onDecrease = {()=> setRed(red-COLOR_INCREMENT)}
                 
-                onIncrease={() => setColor('red', COLOR_INCREMENT)} // set 'red' as string
-                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
+                onIncrease={() => } // set 'red' as string
+                onDecrease={() => }
                 color ="Red" 
                 />
             <ColorCounter 
-                onIncrease = {() => setColor('green', COLOR_INCREMENT)} // set 'green' as string
-                onDecrease = {() => setColor('green', -1 * COLOR_INCREMENT)}
+                onIncrease = {() => } // set 'green' as string
+                onDecrease = {() => }
                 color ="Green"
                 />
             <ColorCounter 
-                onIncrease = {() => setColor('blue', COLOR_INCREMENT)} // set 'blue' as string
-                onDecrease = {() => setColor('blue', -1 * COLOR_INCREMENT)}
+                onIncrease = {() => } // set 'blue' as string
+                onDecrease = {() => }
                 color ="Blue"
                 />
             <View 
